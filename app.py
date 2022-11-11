3e2977c4e6063e129ad24314d7e87cfa7cc74f74
Vineet Arora <dhruv17045@iiitd.ac.in>
2022-11-12 03:21:56 +0530

    Completed Endpoint

--- app.py
+++ app.py
@@ -1,38 +1,44 @@
 from distutils.log import debug
 from flask import Flask, request, jsonify
+import final_model
 
 app = Flask(__name__)
 
 @app.route('/predict', methods=["POST"])
 def get_prediction():
-    ret = {}
     year = request.json['year']
     # Check if year is a number
     if not str(year).isnumeric():
         return jsonify({'Error': '"year" should be a numeric value'})
-    ret['year'] = int(year)
+    
+    final_df = final_model.model_output(int(year))
 
     if 'month' in request.json:
         month = request.json['month']
         # Check if month is number between 1 & 12 (included)
-        if not str(month).isnumeric():
-            return jsonify({'Error': '"month" should be a numeric value'})
-        elif int(month) < 1 or int(month) > 12:
-            return jsonify({'Error': 'Invalid month value'})
-        ret['month'] = int(month)
+        if str(month) != 'Summe':
+            if len(str(month)) > 2:
+                month = str(month)[-2:]
+            if not str(month).isnumeric():
+                return jsonify({'Error': '"month" should be a numeric value'})
+            elif int(month) < 1 or int(month) > 12:
+                return jsonify({'Error': 'Invalid month value'})
+            final_df = final_df[final_df['month'] == int(month)]
 
     if 'category' in request.json:
         category = request.json['category']
         if category not in ['Verkehrsunfälle', 'Alkoholunfälle', 'Fluchtunfälle']:
             return jsonify({'Error': 'Invalid category'})
-        ret['category'] = category
+        final_df = final_df[final_df['category'] == category]
+        
     if 'type' in request.json:
         type = request.json['type']
         if type not in ['insgesamt', 'Verletzte und Getötete', 'mit Personenschäden']:
             return jsonify({'Error': 'Invalid type'})
-        ret['type'] = type
+        final_df = final_df[final_df['type'] == type]
     
-    return jsonify(ret)
+    prediction = int(final_df['prediction'].sum())
+    return jsonify({'prediction': prediction})
 
 if __name__ == '__main__':
     app.run(debug = True)
