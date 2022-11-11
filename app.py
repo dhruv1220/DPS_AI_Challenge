7575ae9eec38d2dbfcf71803a89b3e60769d736b
Vineet Arora <dhruv17045@iiitd.ac.in>
2022-11-11 14:49:31 +0530

    Input Validation Checks in app

--- app.py
+++ app.py
@@ -4,12 +4,35 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/predict', methods=["POST"])
-def add_guide():
+def get_prediction():
+    ret = {}
     year = request.json['year']
+    # Check if year is a number
+    if not str(year).isnumeric():
+        return jsonify({'Error': '"year" should be a numeric value'})
+    ret['year'] = int(year)
+
     if 'month' in request.json:
         month = request.json['month']
-        return jsonify({'content': month})
-    return jsonify({'content_year':'month not present'})
+        # Check if month is number between 1 & 12 (included)
+        if not str(month).isnumeric():
+            return jsonify({'Error': '"month" should be a numeric value'})
+        elif int(month) < 1 or int(month) > 12:
+            return jsonify({'Error': 'Invalid month value'})
+        ret['month'] = int(month)
+
+    if 'category' in request.json:
+        category = request.json['category']
+        if category not in ['Verkehrsunfälle', 'Alkoholunfälle', 'Fluchtunfälle']:
+            return jsonify({'Error': 'Invalid category'})
+        ret['category'] = category
+    if 'type' in request.json:
+        type = request.json['type']
+        if type not in ['insgesamt', 'Verletzte und Getötete', 'mit Personenschäden']:
+            return jsonify({'Error': 'Invalid type'})
+        ret['type'] = type
+    
+    return jsonify(ret)
 
 if __name__ == '__main__':
     app.run(debug = True)
